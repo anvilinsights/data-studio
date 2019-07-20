ddf73a5d8aed3e2465f86cdc34c50f43535f8315
Jon Schwab <jonschwab@MacBook-Pro-6.attlocal.net>
2019-07-20 11:16:03 -0500

    Initial heatmap variables

--- campaign-performance/src/components/Campaign.js
+++ campaign-performance/src/components/Campaign.js
@@ -2,29 +2,51 @@
 import { css, jsx } from '@emotion/core'
 import React from 'react'
 import PropTypes from 'prop-types'
+import Color from 'color'
 
 import { expectedPercentage } from '../utils/stats'
 
 const Campaign = ({ name, clicks, notClicked, expected }) => {
   const exPct = expectedPercentage(expected, clicks)
-  const padded = css`
-    background: #dfdfdf;
-    padding: 5px;
+
+  const baseColor = '#4da3eb'
+  const basePadding = '10px'
+  const multiplier = 0.15
+
+  const adjustedColor =
+    exPct > 1
+      ? Color(baseColor).lighten(exPct * multiplier)
+      : Color(baseColor).darken(exPct * multiplier)
+
+  const expectedStyle = css`
+    background: #999;
+    padding: ${basePadding};
+  `
+
+  const rowStyle = css`
+    background: ${adjustedColor.hsl().string()};
+    color: ${adjustedColor.isLight() ? '#000' : '#333'};
+  `
+
+  const cellStyle = css`
+    padding: ${basePadding};
   `
 
   return (
-    <tr role="row">
-      <td role="cell">{name}</td>
-      <td role="cell" align="right">
+    <tr role="row" css={rowStyle}>
+      <td role="cell" css={cellStyle}>
+        {name}
+      </td>
+      <td role="cell" css={cellStyle} align="right">
         {clicks.toLocaleString()}
       </td>
-      <td role="cell" align="right">
+      <td role="cell" css={cellStyle} align="right">
         {expected.toLocaleString()}
       </td>
-      <td role="cell" align="right">
+      <td role="cell" css={cellStyle} align="right">
         {notClicked.toLocaleString()}
       </td>
-      <td role="cell" css={padded} align="right">
+      <td role="cell" css={expectedStyle} align="right">
         {exPct.toFixed(3)}
       </td>
     </tr>
