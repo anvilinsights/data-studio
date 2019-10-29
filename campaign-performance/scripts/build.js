47edaa22d7de6a38ba32eb07bf967aa5a87fb69a
Jon Schwab <jonschwab@MacBook-Pro-6.local>
2019-10-29 08:36:48 -0500

    Adding description and package URL

--- campaign-performance/scripts/build.js
+++ campaign-performance/scripts/build.js
@@ -1,8 +1,8 @@
-const bluebird = require('bluebird');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const fs = require('mz/fs');
-const path = require('path');
-const webpack = require('webpack');
+const bluebird = require('bluebird')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const fs = require('mz/fs')
+const path = require('path')
+const webpack = require('webpack')
 
 const buildOptions = buildValues => {
   // common options
@@ -37,50 +37,50 @@ const buildOptions = buildValues => {
         },
       ]),
     ],
-  };
+  }
 
   if (buildValues.devMode) {
     const devOptions = {
       mode: 'development',
-    };
-    Object.assign(webpackOptions, devOptions);
+    }
+    Object.assign(webpackOptions, devOptions)
   } else {
     const prodOptions = {
       mode: 'production',
-    };
-    Object.assign(webpackOptions, prodOptions);
+    }
+    Object.assign(webpackOptions, prodOptions)
   }
 
-  return webpackOptions;
-};
+  return webpackOptions
+}
 
 const build = async devMode => {
-  const devBucket = process.env.npm_package_dsccViz_gcsDevBucket;
-  const prodBucket = process.env.npm_package_dsccViz_gcsProdBucket;
-  const deployBucket = devMode ? devBucket : prodBucket;
+  const devBucket = process.env.npm_package_dsccViz_gcsDevBucket
+  const prodBucket = process.env.npm_package_dsccViz_gcsProdBucket
+  const deployBucket = devMode ? devBucket : prodBucket
 
-  const encoding = 'utf-8';
-  const webpackOptions = buildOptions(devMode);
-  const compiler = webpack(webpackOptions);
+  const encoding = 'utf-8'
+  const webpackOptions = buildOptions(devMode)
+  const compiler = webpack(webpackOptions)
 
-  const compilerRun = bluebird.promisify(compiler.run, {context: compiler});
+  const compilerRun = bluebird.promisify(compiler.run, { context: compiler })
 
-  await compilerRun();
+  await compilerRun()
 
-  const manifestSrc = path.resolve(process.env.PWD, 'src', 'manifest.json');
-  const manifestDest = path.resolve(process.env.PWD, 'build', 'manifest.json');
-  const manifestContents = await fs.readFile(manifestSrc, encoding);
+  const manifestSrc = path.resolve(process.env.PWD, 'src', 'manifest.json')
+  const manifestDest = path.resolve(process.env.PWD, 'build', 'manifest.json')
+  const manifestContents = await fs.readFile(manifestSrc, encoding)
   const newManifest = manifestContents
     .replace(/YOUR_GCS_BUCKET/g, deployBucket)
-    .replace(/"DEVMODE_BOOL"/, `${devMode}`);
+    .replace(/"DEVMODE_BOOL"/, `${devMode}`)
 
-  return fs.writeFile(manifestDest, newManifest);
-};
+  return fs.writeFile(manifestDest, newManifest)
+}
 
 const main = () => {
-  const devArg = process.argv[2];
-  const devMode = devArg === 'dev' ? true : false;
-  build(devMode);
-};
+  const devArg = process.argv[2]
+  const devMode = devArg === 'dev' ? true : false
+  build(devMode)
+}
 
-main();
+main()
