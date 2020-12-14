74107e482164ed56e3a462fedd9f46de41178f4b
Kyle McCarthy <kyle@creativeanvil.com>
2020-12-14 16:54:04 -0600

    feat: add dev version for teamwork connector

---
+++ connectors/teamwork/build.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const child_process = require('child_process');
+const exec = child_process.execSync;
+
+const deployDev = async () => {
+  const dev_deployment_id = process.env.npm_package_dsccConnector_dev;
+
+  if (dev_deployment_id === undefined) {
+    throw new Error(
+      'Deployment ID missing from package.json file OR build.js was not run using yarn or npm.'
+    );
+  }
+
+  const buffer = exec(`npx @google/clasp`);
+  const out = buffer.toString('utf8');
+};
+
+const main = async () => {
+  const command = process.argv[2];
+
+  if (command === 'deploy_dev') {
+    await deployDev();
+  }
+};
+
+main();
